daf0b7353406225af10d47ca6f3d61fb365d7ed6
Saleeshadenuwansl <nonesaliya@gmail.com>
2024-12-24 16:40:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "7jAjiBbC#AxmUFu7empfsnMg2RgdZBQFk9H0eUNLBjI2GrxEp684", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
